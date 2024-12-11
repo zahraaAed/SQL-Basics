07aa3de81086d471e54b999bd6a377662dc87491
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-12-11 17:22:38 +0200

    creating table

--- answers.sh
+++ answers.sh
@@ -3,17 +3,42 @@ SELECT name FROM students;
 #answer2-Get all the data of students above 30 years old
 SELECT * FROM students WHERE age > 30;
 #answer3-Get the names of the females who are 30 years old
-SELECT Name FROM Students WHERE Age = 30 AND Gender = 'F';
+SELECT Name FROM students WHERE Age = 30 AND Gender = 'F';
 #answer4-Get the number of Points of Alex
-SELECT Points FROM Students WHERE Name = 'Alex';
+SELECT Points FROM students WHERE Name = 'Alex';
 #answer5-Add yourself as a new student (your name, your age...)
-INSERT INTO Students (ID,Name, Age, Gender, Points)
+INSERT INTO students (ID,Name, Age, Gender, Points)
 VALUES (6,'Zahraa', 22, 'Female', 130);
 #answer6-Increase the points of Basma because she solved a new exercise
-UPDATE Students
+UPDATE students
 SET Points = Points + 23
 WHERE Name = 'Basma';
 #answer7-Decrease the points of Alex because he's late today
-UPDATE Students
+UPDATE students
 SET Points = Points - 14
 WHERE Name = 'Alex';
+
+
+#creating table graduates
+CREATE TABLE graduates (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+    Graduation Text
+);
+
+
+
+#add layal's data
+INSERT INTO graduates (Name, Age, Gender, Points)
+SELECT Name, Age, Gender, Points FROM students WHERE name = 'Layal';
+
+#add layal's graduation date
+UPDATE graduates 
+SET Graduation = '2018-09-08' 
+WHERE Name = 'Layal';
+
+#remove layal's data
+DELETE FROM students WHERE Name = 'Layal';
